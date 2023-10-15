7ec25632792affe1e184287447781eae06fa6f70
Dell <backenddjangopy3@gmail.com>
2023-10-15 21:08:45 +0100

    Explain the perpose of the post fields

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    title = models.TextField()
+
+    title = models.CharField(max_length=250) # => this field translate to VARCHAR column in the SQL database.
+    slug = models.SlugField(max_length=250) # => this field translate to VARCHAR column in the SQL database. Slug is a short label contains only letters,numbers, underscore, hypens
+    title = models.TextField() # => 
     def __str__(self):
         return f'[{self.title}]'
 

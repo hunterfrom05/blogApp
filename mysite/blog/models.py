fc6271ec2e71baa8a0481d787a1cdc11a2346133
Dell <backenddjangopy3@gmail.com>
2023-10-15 20:23:05 +0100

    Creating the post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    title = models.TextField()
+    def __str__(self):
+        return f'[{self.title}]'
+        
